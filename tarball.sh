889ddd2e81266ee42a21efa5c14b1240530a848f
Dor Galam <dorg@wix.com>
2017-12-22 14:56:28 +0200

    Changed .sh to exclude .gitignore aswell

--- tarball.sh
+++ tarball.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 mkdir 311232268
 cp -a $1 311232268
-tar -cvf 311232268.tar --exclude .git --exclude .vscode --gzip ./311232268
+tar -cvf 311232268.tar --exclude .git --exclude .gitignore --exclude .vscode --gzip ./311232268
 rm -rf 311232268/
