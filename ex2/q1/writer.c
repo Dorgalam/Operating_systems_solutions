bc5834be3d9c464b773b6552cbebda42a0468e67
Dor Galam <dorg@wix.com>
2017-12-23 17:36:37 +0200

    made changes to writer.c to make sure buffer is empty when terminating

--- ex2/q1/writer.c
+++ ex2/q1/writer.c
@@ -6,16 +6,24 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
+void flushBuff();
+
+// Makes the program a bit more complex but this make sures
+// the buffer is flushed before terminating this program, even if
+// it was terminated by an interrupt
+
+void flushBuff() {
+  fflush(stdout);
+  exit(EXIT_SUCCESS); 
+}
+
 int main(int argc, char **argv) {
   int fileOut;
+  signal(SIGINT, flushBuff);
   if (argc > 1 && strcmp(argv[1], "std") != 0) {
-    if ((fileOut = open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0666)) == -1) {
+    close(1);
+    if (open(argv[1], O_WRONLY|O_CREAT|O_TRUNC, 0666) == -1) 
       exit(EXIT_FAILURE);
-    } else {
-      close(1);
-      dup(fileOut);
-      close(fileOut);
-    }
   }
   char ch;
   while ((ch = getchar()) != EOF) 
